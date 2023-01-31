ad1558d0739319d88ebb623359a7f635b134de81
Abdullah BerkayUymaz <110728400+berkayuymaz0@users.noreply.github.com>
2023-01-31 15:15:52 +0300

    script

---
+++ servo.py
@@ -0,0 +1,23 @@
+#door open/close servo motor control script for jetson nano
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+
+p = GPIO.PWM(18, 50)
+p.start(0)
+
+try:
+    while True:
+        p.ChangeDutyCycle(7.5)
+        time.sleep(1)
+        p.ChangeDutyCycle(12.5)
+        time.sleep(1)
+        p.ChangeDutyCycle(2.5)
+        time.sleep(1)
+        
+except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
